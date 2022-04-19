99a036e0f40bcdf0724cf202d66d486081d45562
Sandro Bistene <sandrobistene@Sandros-MacBook-Pro.local>
2022-04-19 14:13:00 -0300

    feat: select operator ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,29 @@ export default function Header() {
   return (
     <header>
       <h1> Star Wars Planets Search</h1>
-      <input
-        name="nameInput"
-        type="text"
-        value={ handleInput }
-        data-testid="name-filter"
-        onChange={ (e) => filteredInformation(e) }
-      />
+      <form>
+        <label htmlFor="nameInput">
+          Search by name:
+          <input
+            name="nameInput"
+            id="nameInput"
+            type="text"
+            value={ handleInput }
+            data-testid="name-filter"
+            onChange={ (e) => filteredInformation(e) }
+          />
+        </label>
+        <label htmlFor="column-filter">
+          Choose an operator:
+          <select name="column-filter" id="column-filter" data-testid="column-filter">
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+        </label>
+      </form>
     </header>
   );
 }
